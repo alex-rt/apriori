0644bb27cef217de0367c87c25e79452a832d961
alex-rt <61000223+alex-rt@users.noreply.github.com>
2021-11-04 18:45:28 -0600

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -1,19 +1,43 @@
-T = [{'a','c','d'},{'b','c','e'},{'a','b','c','e'},{'b','e'}]
-print(T)
-C1= [{'a'},{'b'},{'c'},{'d'},{'e'}]
-Lk = []
+T = [['A', 'C', 'D'],
+    ['B', 'C', 'E'],
+  ['A', 'B', 'C', 'E'],
+  ['B', 'E']]
+
+C=[['A','B','C','D','E'][['A','B'],['A','C'],['A','E'],['B','C'],['B','E'],['C','E']],
+['B','C','E']]
+L=[[],[],[],[]]
 min_sup = 2
 count = 0
+k=0
+
 for i in T:
   for i in i:
     print(i)
 
-for i in T:
-  for i in i:
-    if {"i"} in C1 == True:
-      count = count +1
-    else:
-      C1.append({i})
+def countOccurrences(my_list, term):
+  counts = {}
+  
+  for row in my_list:
+    if row[term] not in counts:
+      counts[row[term]] = 0
+    counts[term] += 1
+  return counts
 
-print(count)
-print(C1)
+while k>3:
+  for item in T:
+    if item in C[k] == True:
+      C[k].append(item)
+      k=k+1
+
+k2=0
+while k2>3:
+  for candidate in C:
+    ocurrences = countOcurrences(C, candidate)
+    if ocurrences> min_sup:
+      L[k2].append(candidate)
+      
+min_sup = 2
+
+
+
+
